50a7afbca7c572670bcdde20a33771cec9006e9e
Siras <sirasudeenp@gmail.com>
2025-03-21 02:25:49 -0700

    Initial Commit

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+    return (
+        <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
+            <div className='mb-4 text-4xl font-mono font-bold'>
+                Loading
+            </div>
+        </div>
+    )
+}
+
+export default LoadingScreen
